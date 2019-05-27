96e6f5369566ef8be16de935b2ad4af087bc52a8
miguel17546mals <miguel17546mals@gmail.com>
2019-05-26 23:04:04 -0500

    Agrego el archivo central que se refiere a la logica principal

--- Central.vhd
+++ Central.vhd
@@ -3,6 +3,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE ieee.numeric_std.ALL;
 
 entity central is
 			 --Entradas
@@ -12,25 +13,64 @@ entity central is
 			  Premium : in std_logic;
 			  --Salidas
            bombaMagna   : out std_logic;
-           BombaPremium : out std_logic);
+           bombaPremium : out std_logic);
 end central;
 
 architecture Behavioral of central is
 --Declaracion de las variables que manejaremos en el proceso
 signal litros : integer;
 signal precio : real; 
-signal contador : integer;
-
+signal contador :  integer range 0 to 999;
+--signal contador : integer;
+TYPE estados is (S0,S1,S2,S3,S4,S5,S6,S7,S8,S9);
+Signal estado_pr,estado_sig : estados;
 begin
  litros <= 0;
  precio <= 0.0;
-	 process is 
-	  variable i : integer := 0;
-	 begin
-		 for i in 0 to 999 loop
-			exit when i = litros;
-			wait for 1 sec;
-		 end loop;
-	 end process;
-	 
+ contador <= 0;
+ --Inicializacion de los estados
+ process (clk,KeyPad) begin 
+	if (KeyPad = "1101") then 
+		estado_pr <= S0;
+	elsif (clk'event and clk ='1') then
+		estado_pr <= estado_sig;
+	end if;
+ end process;
+ 
+ --Definicion de los estados
+ process (KeyPad,estado_pr,contador) begin
+	case(estado_pr) is 
+		when S0 =>
+			bombaMagna <= '0';
+			bombaPremium <= '0';
+			if (KeyPad = "0000" or KeyPad = "0001" or KeyPad = "0010" or KeyPad = "0011" or KeyPad = "0100"
+			or KeyPad = "0101" or KeyPad = "0110" or KeyPad = "0111" or KeyPad = "1000" or KeyPad = "1001") then
+				estado_sig <= S1;
+				contador <= CONV_INTEGER(KeyPad);
+			else 
+				estado_sig <= S0;
+			end if;
+		when S1 =>
+			bombaMagna <= '0';
+			bombaPremium <= '0';
+			if (KeyPad = "0000" or KeyPad = "0001" or KeyPad = "0010" or KeyPad = "0011" or KeyPad = "0100"
+			or KeyPad = "0101" or KeyPad = "0110" or KeyPad = "0111" or KeyPad = "1000" or KeyPad = "1001") then
+				estado_sig <= S2;
+			else 
+				estado_sig <= S0;
+			end if;
+		when S2 =>
+			bombaMagna <= '0';
+			bombaPremium <= '0';
+			if (KeyPad = "0000" or KeyPad = "0001" or KeyPad = "0010" or KeyPad = "0011" or KeyPad = "0100"
+			or KeyPad = "0101" or KeyPad = "0110" or KeyPad = "0111" or KeyPad = "1000" or KeyPad = "1001") then
+				estado_sig <= S3;
+			else 
+				estado_sig <= S0;
+			end if;			
+	end case;
+ end process;
+ 
+ 
+
 end Behavioral;
--- simulation/modelsim/ProyectoHD.vho
+++ simulation/modelsim/ProyectoHD.vho
@@ -16,7 +16,7 @@
 -- PROGRAM "Quartus II 64-Bit"
 -- VERSION "Version 13.0.1 Build 232 06/12/2013 Service Pack 1 SJ Web Edition"
 
--- DATE "05/26/2019 03:40:13"
+-- DATE "05/26/2019 22:38:35"
 
 -- 
 -- Device: Altera EP4CGX15BF14C6 Package FBGA169
